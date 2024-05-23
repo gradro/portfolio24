8722d0b2e8e3ed12c3d75bcc6a880fa38f1cbe38
gradro <drozdek.gracjan@gmail.com>
2024-05-23 16:13:42 +0200

    changed delay

--- components/AnimatedHeading.tsx
+++ components/AnimatedHeading.tsx
@@ -16,8 +16,8 @@ const defaultAnimations = {
         opacity: 1,
         y: 0,
         transition: {
-          delay: 0.15 * index,
-          duration: 0.4
+          delay: 0.1 * index,
+          duration: 0.3
         }
     })
 }
