9093bfb03b1854eae577a8355d9394cd1b19075d
gradro <drozdek.gracjan@gmail.com>
2024-05-26 23:22:19 +0200

    contact button

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,9 +1,26 @@
+"use client"
+import Button from "@/components/Button"
+import Form from "@/components/Form/Form"
 
 const Contact = () => {
+
+  const submitForm = () => {
+    // Submit
+  }
+
   return (
     <section className="container">
-        <p className="uppercase text-center font-medium opacity-80 pb-1">Drop me a Line</p>
-        <h2 className="text-center">Let's <span className="text-highlight">Create</span> Something Great Together</h2>
+      <p className="uppercase text-center font-medium opacity-80 pb-1">Drop me a Line</p>
+      <h2 className="text-center">Let's <span className="text-highlight">Create</span> Something Great Together</h2>
+      <div className="text-center">
+        <Button 
+          variant="outline"
+          href="mailto:drozdek.gracjan@gmail.com"
+        >
+          Contact me
+        </Button>
+      </div>
+      {/* <Form submit={submitForm}/> */}
     </section>
   )
 }
