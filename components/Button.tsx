a3671987cf2ccd88f7b0c9962664d74b7e2c3ca4
gradro <drozdek.gracjan@gmail.com>
2024-05-26 23:21:22 +0200

    download prop

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,11 +2,12 @@ type ButtonProps = {
     as?: keyof JSX.IntrinsicElements;
     children: React.ReactNode
     href: string
-    variant: "primary" | "outline"
+    variant: "primary" | "outline",
+    download?: boolean,
     onClick?: () => void;
 }
 
-const Button = ({ as, children, href, variant="primary", onClick }: ButtonProps) => {
+const Button = ({ as, children, href, variant="primary", onClick, download }: ButtonProps) => {
 
     const Tag = as || "a";
     const variants = {
@@ -15,7 +16,7 @@ const Button = ({ as, children, href, variant="primary", onClick }: ButtonProps)
     }
 
     return (
-        <Tag onClick={onClick} href={href || "#"} className={`px-8 py-3 font-medium cursor-pointer ${variants[variant]}`}>
+        <Tag onClick={onClick} href={href || "#"} download={download} className={`rounded-sm px-8 py-3 font-medium cursor-pointer ${variants[variant]}`}>
             {children}
         </Tag>
     )
